c95f655e5a3fc85d1508399de5407a73df3eaa3e
Nan Wang <wangnangg@gmail.com>
2020-04-06 13:36:22 -0700

    scroll

--- config.h
+++ config.h
@@ -176,6 +176,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
+	{ MODKEY,               XK_u,     		kscrollup,      {.i = -1} },
+	{ MODKEY,               XK_d,   		kscrolldown,    {.i = -1} },
+	{ MODKEY,               XK_k,     		kscrollup,      {.i =  1} },
+	{ MODKEY,               XK_j,   		kscrolldown,    {.i =  1} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 };
